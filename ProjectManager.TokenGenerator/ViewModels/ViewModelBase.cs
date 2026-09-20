d7afab99448b7b7b06db115e50800c78d08fbc25
xTfTV <mscott2003@icloud.com>
2026-09-20 15:20:07 -0400

    Initial commit of the token generator

---
+++ ProjectManager.TokenGenerator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProjectManager.TokenGenerator.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ ProjectManager.TokenGenerator/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProjectManager.TokenGenerator.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
